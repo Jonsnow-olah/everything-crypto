e7592cc0fe863bb5e6dbfd7f3fa5d69622b5f04b
Olah Israel <olahisrael07@gmail.com>
2023-03-08 00:44:38 +0100

    "second commit"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,68 +3,63 @@ import React from 'react'
 import { BsTelegram, BsTwitter, BsMedium } from "react-icons/bs";
 // import { FaTiktok } from "react-icons/fa";
 
-const links = [
-    {
-      title: "About",
-      data: ["About", "Terms", "Legal"],
-    },
-    // {
-    //   title: "NFT",
-    //   data: ["OpenSea", "Maker", "Learn"],
-    // },
-    {
-      title: "Contact",
-      data: ["Support"],
-    },
-    {
-      title: "Social",
-      data: ["Twiiter", "Instagram"],
-    },
-  ];
+// const links = [
+//     {
+//       title: "About",
+//       data: ["About", "Terms", "Legal"],
+//     },
+//     // {
+//     //   title: "NFT",
+//     //   data: ["OpenSea", "Maker", "Learn"],
+//     // },
+//     {
+//       title: "Contact",
+//       data: ["Support"],
+//     },
+//     {
+//       title: "Social",
+//       data: ["Twiiter", "Instagram"],
+//     },
+//   ];
 
-  const socialLink = [
-    <BsTwitter />,
-    <BsMedium />,
-    <BsTelegram />,
-  ];
+  // const socialLink = [
+  //   <BsTwitter onClick={() => {}} />,
+  //   <BsMedium />,
+  //   <BsTelegram />,
+  // ];
 
 function Footer() {
   return (
     <footer>
-        <div className='upper'>
-            <div className='brand-container'>
-                <div className='brand'>
-                    {/* <img src={logo} alt='logo' /> */}
-                </div>
-                <p>Exclusive Nft collevtion</p>
-                <ul>
-                    {
-                        socialLink.map((link, index) => (
-                            <li key={index}>{link}</li>
-                        ))
-                    }
-                </ul>
-            </div>
-            <div className="links">
-          {links.map(({ title, data }, index) => {
-            return (
-              <div className="link" key={index}>
-                <h4>{title}</h4>
-                <ul>
-                  {data.map((link, index2) => (
-                    <li key={index2}>{link}</li>
-                  ))}
-                </ul>
+          <div className='upper'>
+              <div className='brand-container'>
+                  <div className='brand'>
+                      {/* <img src={logo} alt='logo' /> */}
+                  </div>
+                  <p>Our Social links</p>
+                  <ul>
+                    <a href='medium.com'><li><BsMedium /></li></a>
+                    <a href='medium.com'><li><BsTwitter /></li></a>
+                    <a href='medium.com'><li><BsTelegram /></li></a>
+                      {/* {
+                          socialLink.map((link, index) => (
+                              <li key={index}>{link}</li>
+                          ))
+                      } */}
+                  </ul>
               </div>
-            );
-          })}
-        </div>
-      </div>
-      <div className="lower">
-        <span>&copy; Copyright 2022 Olah</span>
+              <div className="links">
+                <h4 href="#home">About Us</h4>
 
+              </div>
+
+              
         </div>
-    </footer>
+        <div className="lower">
+          <span>&copy; Copyright 2022 Olah</span>
+
+          </div>
+      </footer>
   )
 }
 
